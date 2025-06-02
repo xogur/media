cd88cbc51574ddd23eb62f231a2e2d7c10f02ee0
xogur <snsk656@naver.com>
2025-06-02 21:19:35 +0900

    125

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -11,7 +11,9 @@ public class SocialUser {
     // 자동으로 증가되는 값
     private Long id;
 
+    // SocialProfile 엔티티의 user 필드에 의해 매핑된 양방향 OneToOne 관계의 주인이 아님을 의미
     @OneToOne(mappedBy = "user")
     //@JoinColumn(name = "social_profile_id")
+    // 관계의 주인이 아니므로 외래키 설정을 직접 하지 않음 (주인은 SocialProfile임)
     private SocialProfile socialProfile;
 }
