57bbd4959ca1cd2121624641bbe714be1c850b47
xogur <snsk656@naver.com>
2025-06-02 21:09:23 +0900

    2

--- src/main/java/com/social/media/models/SocialProfile.java
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -4,12 +4,20 @@ import jakarta.persistence.*;
 
 @Entity
 //엔티티를 정의
+//SocialProfile은 DB의 테이블
+//@Entity 해당 어노테이션 덕분에 아래 클래스를 테이블로 인식
 public class SocialProfile {
     @Id
+    // @Id는 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 자동 증가하는 정수 값
     private Long id;
 
     @OneToOne
+    // SocialProfile과 SocialUser 간에 1:1 관계임을 의미합니다.
+    // 예: 하나의 유저는 하나의 프로필을 가진다.
     @JoinColumn(name = "social_user")
+    // 이 테이블(social_profile)에 social_user라는 외래키(FK) 컬럼을 만들어 SocialUser 엔티티의 id와 연결합니다.
+    // 참조하는 엔티티 (SocialUser)의 기본키(@Id) 에 자동으로 연결한다.
     private SocialUser user;
 }
--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,14 +1,17 @@
 package com.social.media.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-//?
 @Entity
+// 해당 클래스를 DB의 테이블로 인식
 public class SocialUser {
     @Id
+    // 해당 변수를 기본키로
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 자동으로 증가되는 값
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    //@JoinColumn(name = "social_profile_id")
+    private SocialProfile socialProfile;
 }
