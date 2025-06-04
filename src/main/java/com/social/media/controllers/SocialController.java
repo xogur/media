8cb9b6c4570fe12f17bd8b83dac734a74ff9f1ff
xogur <snsk656@naver.com>
2025-06-04 17:59:01 +0900

    13２

--- src/main/java/com/social/media/controllers/SocialController.java
+++ src/main/java/com/social/media/controllers/SocialController.java
@@ -5,11 +5,7 @@ import com.social.media.services.SocialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -28,5 +24,11 @@ public class SocialController {
     // socialUser 안에 socialProfile을 하위 객체로 저장하는 경우
     public ResponseEntity<SocialUser> saveUser(@RequestBody SocialUser socialUser){
         return new ResponseEntity<>(socialService.saveUser(socialUser), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/social/users/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId){
+        socialService.deleteUser(userId);
+        return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -20,7 +20,7 @@ public class SocialUser {
     private Long id;
 
     // SocialProfile 엔티티의 user 필드에 의해 매핑된 양방향 OneToOne 관계의 주인이 아님을 의미
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     //@JoinColumn(name = "social_profile_id")
     // 관계의 주인이 아니므로 외래키 설정을 직접 하지 않음 (주인은 SocialProfile임)
     private SocialProfile socialProfile;
